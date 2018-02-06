f350c529015c23a67f77d56aa7d105b71fb0ff60
kazuki <kazu2055.tennis@gmail.com>
2018-02-07 00:47:06 +0900

    generate user and email

--- fake_accounts.py
+++ fake_accounts.py
@@ -3,23 +3,27 @@ import random
 
 from faker import Faker
 
+fake = Faker()
 
-def create_password():
+
+def gen_password():
+    password = ""
     strings = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()*+,-./:;<=>?@[]^_`{|}~"
     pass_len = random.randint(8, 16)
-    mypw = ""
-
     for i in range(pass_len):
-        mypw += strings[random.randrange(len(strings))]
+        password += strings[random.randrange(len(strings))]
+    return password
 
-    print(mypw)
+
+def gen_user():
+    email = fake.email()
+    username = email[:email.index("@")]
+    return username, email
 
 
 def main():
-    # fake = Faker()
-    # for i in range(3):
-    #     print(fake.email())
-    create_password()
+    print(gen_user())
+    print(gen_password())
 
 
 if __name__ == "__main__":
